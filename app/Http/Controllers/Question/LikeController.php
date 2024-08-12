2f3265103bff8dc379ae2984f29c597d63a73f32
Rafael <Rafaelrgsv@gmail.com>
2024-08-12 16:49:41 -0300

    hey-11: refactoring to create an action method on user model to like a question

--- app/Http/Controllers/Question/LikeController.php
+++ app/Http/Controllers/Question/LikeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Question;
 
 use App\Http\Controllers\Controller;
-use App\Models\{Question, Vote};
+use App\Models\{Question};
 use Illuminate\Http\{RedirectResponse};
 
 class LikeController extends Controller
@@ -12,12 +12,8 @@ class LikeController extends Controller
     // Pega o id que foi passado pela rota e executa um find na Model especificada (Question) para procurar a questão com esse id
     public function __invoke(Question $question): RedirectResponse
     {
-        Vote::query()->create([
-            'question_id' => $question->id, //
-            'user_id'     => auth()->id(),
-            'like'        => 1,
-            'unlike'      => 0,
-        ]);
+        // Diz que o usuário gostou da pergunta
+        auth()->user()->like($question);
 
         // Faz com que retorne para o ponto em que estava antes de chamar essa função estava
         return back();
